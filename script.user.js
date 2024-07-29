14c31797d5f2408328458a6b556273323bcc0c33
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-07-29 11:21:01 +0200

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Script
+// @namespace    BBBaden-Moodle-userscripts
+// @version      1.0.0
+// @author       black-backdoor
+// @match        *://*/*
+// @grant        GM_info
+// @grant        GM.info
+// @note         MODULES: [bridge.lib.js]
+// @require      https://github.com/BBBaden-Moodle-userscripts/BBBUserScriptManager/raw/main/bridge.lib.js
+// ==/UserScript==
+
+
+const connection = new Script();
